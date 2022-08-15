18b98d05c8a9ecdb10284da666212f55cefcfb4c
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2022-08-15 02:48:47 -0700

    ternary operators

---
+++ Operators/TernaryOperators/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("Hello world! Was Car is true!");
+        }
+    }
+}
