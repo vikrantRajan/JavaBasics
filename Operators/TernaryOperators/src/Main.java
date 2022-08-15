73616715db18c7fd839e0d893aac9015b4c26d2e
Vikrant Rajan <vikrant_rajan@thecdm.ca>
2022-08-15 02:59:37 -0700

    operator precedence

--- Operators/TernaryOperators/src/Main.java
+++ Operators/TernaryOperators/src/Main.java
@@ -5,5 +5,14 @@ public class Main {
         if (wasCar) {
             System.out.println("Hello world! Was Car is true!");
         }
+
+
+        double one = 20.00;
+        double two = 80.00;
+
+        double result = ((one + two) * 100) % 40;
+        boolean ans = result == 0 ? true : false;
+        if (ans) System.out.println("No remainder... result = " + result);
+        else System.out.println("No remainder, result = " + result);
     }
 }
